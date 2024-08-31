f1f44b75fb9e5c3216058a4326582ac2954d2b35
jamesjo <jamescakr@gmail.com>
2024-08-31 07:38:18 -0600

    netflix lec6 &7

--- src/pages/Homepage/Homepage.jsx
+++ src/pages/Homepage/Homepage.jsx
@@ -1,11 +1,13 @@
-import React from 'react'
+import React from "react";
+import Banner from "./components/Banner/Banner";
 
 const Homepage = () => {
   return (
     <div>
       Homepage
+      <Banner />
     </div>
-  )
-}
+  );
+};
 
-export default Homepage
+export default Homepage;
